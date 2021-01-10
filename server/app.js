c8ff8ac80c0c4fb2d1f5c5e3e48451a87a6e90c6
tomertv2 <tomertv2@gmail.com>
2021-01-10 23:12:41 +0200

    Add mongoDB

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,18 @@ const app = express();
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 app.use('/api', require('./api'));
 
