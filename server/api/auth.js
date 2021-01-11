6bb31c71883b91c06bac1dfffd5296e508a14a18
tomertv2 <tomertv2@gmail.com>
2021-01-11 19:17:14 +0200

    Add login with jwt

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const auth = require('../middlewares/auth');
 
 const router = Router();
 
@@ -8,8 +10,8 @@ const userSchema = require('../validations');
 
 const saltRounds = 10;
 
-// Create new user
-router.post('/', (req, res) => {
+// Register
+router.post('/register', (req, res) => {
   try {
     const { username, password, email } = req.body;
 
@@ -22,9 +24,9 @@ router.post('/', (req, res) => {
     const { error } = userSchema.validate(req.body);
     if (error) return res.status(400).json({ error: error.message });
 
-    userIsExist(username).then((checker) =>
-      checker
-        ? res.status(409).json('User already exists.')
+    userIsExist(username).then((existUser) =>
+      existUser
+        ? res.status(409).json('Username already exists.')
         : newUser.save().then((savedUser) => res.json(savedUser))
     );
   } catch (error) {
@@ -32,11 +34,33 @@ router.post('/', (req, res) => {
   }
 });
 
-// check if user is in the DB
+// Login
+router.post('/login', (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    userIsExist(username).then((existUser) => {
+      if (!existUser) res.status(400).json('Wrong username or password.');
+      
+      bcrypt.compare(password, existUser.password).then((match) => {
+        if (!match) res.status(400).json('Wrong username or password.');
+        
+        const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+          expiresIn: '24h',
+        });
+        return res.json({ token });
+      });
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Check if user is in the DB
 const userIsExist = async (username) => {
   try {
     const user = await User.findOne({ username });
-    return user ? user.toJSON() : false;
+    return user ? user : false;
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
